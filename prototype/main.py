31a74bbf0857468f5c3063e32712574ea5741de7
Nathan Whitehead <nwhitehe@gmail.com>
2026-03-11 13:05:49 -0700

    Loop through data and show it.

--- prototype/main.py
+++ prototype/main.py
@@ -1,8 +1,17 @@
 import pyarrow.parquet as pq
+import numpy as np
+import pandas as pd
+
+TRAIN_FILENAME = './train.parquet'
 
 def main():
-    print("Hello from prototype!")
-
+    table = pq.read_table(TRAIN_FILENAME).to_pandas()
+    for row in range(table.shape[0]):
+        question = table[['question']].iat[row, 0]
+        answer = table[['provided_answer']].iat[row, 0]
+        reference = table[['reference_answer']].iat[row, 0]
+        score = table[['normalized_grade']].iat[row, 0]
+        print(f'Q: {question}\nA: {answer}\nR: {reference}\nS: {score}\n')
 
 if __name__ == "__main__":
     main()
